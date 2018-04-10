4c6f1d964d6673b4a27b95bbe8d4ddce629e0344
Bhupender Singh <bhupenders225@gmail.com>
2018-04-11 01:08:35 +0530

    adding enquiry ui page

--- enquiry/script.js
+++ enquiry/script.js
@@ -1,18 +1,25 @@
-/*
-$('button').click(function () {
-    $(this).toggleClass('clicked');
-    $('button p').text(function (i, text) {
-        return text === "Sent!" ? "Send" : "Sent!";
-    });
-});
-*/
-
-
-
+/* for button animation */
 target = document.querySelector("button")
 targetPara = document.querySelector("button p");
 
 target.addEventListener("click" , function() {
     this.classList.add('clicked');
     targetPara.innerHTML = "Sent";
+})
+
+/* for alert */
+
+var allInput = document.querySelectorAll("input, textarea");
+var alert = document.querySelector(".required");
+var greet = document.querySelector(".greet");
+var send = document.querySelector(".animate_area button");
+
+for(var i=0; i<allInput.length; i++) {
+    allInput[i].addEventListener("click", function() {
+        alert.classList.add("show");
+    })
+}
+
+send.addEventListener("click", function() {
+    greet.classList.add("show");
 })
