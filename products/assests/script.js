266f11939fda139d50c2300bceecfe1a42b504ed
Bhupender Singh <bhupenders225@gmail.com>
2018-04-11 20:49:27 +0530

    updating 1st category linking with enquiry

--- products/assests/script.js
+++ products/assests/script.js
@@ -1,5 +1,13 @@
 /* local storage save */
 
 var currentProductName = document.querySelector("#jumbotron h1").textContent;
+var targetEnquiry = document.querySelectorAll(".enquiry a");
 
-localStorage.setItem("product", currentProductName);
+localStorage.removeItem("product");
+
+for(var i=0; i<targetEnquiry.length; i++) {
+    targetEnquiry[i].addEventListener("click", function() {
+        localStorage.setItem("product", currentProductName);
+    })
+}
+
