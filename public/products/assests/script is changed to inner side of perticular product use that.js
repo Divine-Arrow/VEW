b28922ae5ef68eb645af68f6ab1f93df92503945
Bhupender Singh <bhupenders225@gmail.com>
2018-04-16 14:42:35 +0530

    updatinf directories

--- public/products/assests/script is changed to inner side of perticular product use that.js
+++ public/products/assests/script is changed to inner side of perticular product use that.js
@@ -5,9 +5,8 @@ var targetEnquiry = document.querySelectorAll(".enquiry a");
 
 localStorage.removeItem("product");
 
-for(var i=0; i<targetEnquiry.length; i++) {
-    targetEnquiry[i].addEventListener("click", function() {
+for (var i = 0; i < targetEnquiry.length; i++) {
+    targetEnquiry[i].addEventListener("click", function () {
         localStorage.setItem("product", currentProductName);
     })
 }
-
